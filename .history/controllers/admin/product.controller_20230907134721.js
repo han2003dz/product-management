25fca01abd2264b168d73b9f640e3140bd3dc532
han2003dz <han20031605@gmail.com>
2023-09-07 14:32:18 +0700

    fix pagination

---
+++ .history/controllers/admin/product.controller_20230907134721.js
@@ -0,0 +1,49 @@
+// [GET/admin/product]
+const Product = require("../../models/product.model");
+const filterStatusHelpers = require("../../helpers/filterStatus");
+const searchHelpers = require("../../helpers/search");
+
+module.exports.index = async (req, res) => {
+  // console.log(req.query.status);
+  const filterStatus = filterStatusHelpers(req.query);
+  const objectSearch = searchHelpers(req.query);
+  console.log(filterStatus);
+
+  let find = {
+    deleted: false,
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  if (objectSearch.regex) {
+    find.title = objectSearch.regex;
+  }
+
+  // pagination
+  let objectPagination = {
+    currentPage: 1,
+    limitItemProduct: 4,
+  };
+
+  if (req.query.page) {
+    objectPagination.currentPage = parseInt(req.query.page);
+  }
+
+  objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItemProduct;
+  const countProducts = await Product.co
+  
+  // end pagination
+
+  const products = await Product.find(find).limit(4);
+
+  // console.log(products);
+
+  res.render("admin/pages/product/index", {
+    pageTitle: "Danh sách sản phẩm",
+    products: products,
+    filterStatus: filterStatus,
+    keyword: objectSearch.keyword,
+  });
+};
