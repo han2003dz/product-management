5626ec9e53eceb406c45c3bdd51ec44a8b73c288
han2003dz <han20031605@gmail.com>
2023-09-07 16:08:54 +0700

    fix search

---
+++ .history/helpers/search_20230907160427.js
@@ -0,0 +1,13 @@
+module.exports = (query) => {
+  let objectSearch = {
+    keyword: "",
+  };
+  if (query.keyword) {
+    objectSearch.keyword = query.keyword;
+    const re = new RegExp(objectSearch.keyword, "i");
+    objectSearch.regex = re;
+  }else{
+    ob
+  }
+  return objectSearch;
+};
