207aef1fe8fac6ccb84b5a7b1652ddddb6459e30
han2003dz <han20031605@gmail.com>
2023-09-12 13:06:10 +0700

    thông báo thay đổi trạng thái

---
+++ .history/index_20230912015218.js
@@ -0,0 +1,45 @@
+// import library
+const express = require("express");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const flash = require("express-flash");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+//import config
+const database = require("./config/database");
+require("dotenv").config();
+const systemConfig = require("./config/system");
+
+// import route and port
+const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
+const port = process.env.PORT;
+
+const app = express();
+database.connect();
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+// App local variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// flash
+app.use(express.cookieParser("keyboard cat"));
+app.use(express.session({ cookie: { maxAge: 60000 } }));
+app.use(flash());
+
+// Routes
+route(app);
+routeAdmin(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
