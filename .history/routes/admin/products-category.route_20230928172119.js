0b12e69f9aad8814c82c5e5a2b38c8a90eed8047
han2003dz <han20031605@gmail.com>
2023-09-28 17:31:28 +0700

    delete-xem 1 product-category

---
+++ .history/routes/admin/products-category.route_20230928172119.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const controller = require("../../controllers/admin/products-category.controller");
+const upload = multer();
+const validate = require("../../validates/admin/product-category.validate");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+router.get("/", controller.index);
+// :status truyền data động
+router.patch("/change-status/:status/:id", controller.changeStatus);
+
+router.get("/create", controller.create);
+router.post(
+  "/create",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
+);
+
+router.get("/edit/:id", controller.edit);
+router.patch(
+  "/edit/:id",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.editPatch
+);
+
+router.delete("/delete/:id", controller.deleteItem);
+
+router.get("/")
+
+module.exports = router;
