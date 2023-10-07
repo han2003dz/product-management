221291a85d4e449c1e838d17a94c29eca85df547
han2003dz <han20031605@gmail.com>
2023-10-07 11:19:36 +0700

    thêm log sản phẩm

--- .history/models/product.model_20230829003152.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const product Schema = new product.Schema({
-    
-})
---
+++ .history/models/product.model_20231007110221.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+const mongooseDelete = require("mongoose-delete");
+
+
+const productSchema = new mongoose.Schema(
+  {
+    title: String,
+    product_category_id: {
+      type: String,
+      default: "",
+    },
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    slug: {
+      type: String,
+      slug: "title",
+      unique: true,
+    },
+    createdBy: {
+
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Add plugin
+mongoose.plugin(slug);
+productSchema.plugin(mongooseDelete);
+
+// export model
+const Product = mongoose.model("Products", productSchema, "products");
+module.exports = Product;
