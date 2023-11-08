142f3c75f2a6b52f4ca394b7222954ef7fdac966
han2003dz <han20031605@gmail.com>
2023-11-08 20:38:50 +0700

    khi A hủy lời mời kết bạn, cập nhật lại số lượng lời mời bên B - Hiển thị A vào ds lời mời của B

---
+++ .history/public/js/users_20231108202705.js
@@ -0,0 +1,87 @@
+// chức năng từ chối kết bạn
+const refuseFriend = (button) => {
+  button.addEventListener("click", () => {
+    button.closest(".box-user").classList.add("refuse");
+    const userId = button.getAttribute("btn-refuse-friend");
+    socket.emit("CLIENT_REFUSE_FRIEND", userId);
+  });
+};
+
+// chức năng chấp nhận kết bạn
+const acceptFriend = (button) => {
+  button.addEventListener("click", () => {
+    button.closest(".box-user").classList.add("accepted");
+    const userId = button.getAttribute("btn-accept-friend");
+    socket.emit("CLIENT_ACCEPT_FRIEND", userId);
+  });
+};
+
+// chức năng gửi yêu cầu
+const addFriend = (button) => {
+  button.addEventListener("click", (e) => {
+    // tìm từ thẻ cha là box-user có thẻ con là button thì thêm class add cho button item đó
+    button.closest(".box-user").classList.add("add");
+    const userId = button.getAttribute("btn-add-friend");
+    socket.emit("CLIENT_ADD_FRIEND", userId);
+  });
+};
+
+const cancelFriend = (button) => {
+  button.addEventListener("click", () => {
+    button.closest(".box-user").classList.remove("add");
+    const userId = button.getAttribute("btn-cancel-friend");
+    socket.emit("CLIENT_CANCEL_ADD_FRIEND", userId);
+  });
+};
+
+
+// Chức năng gửi yêu cầu
+const listBtnAddFriend = document.querySelectorAll("[btn-add-friend]");
+if (listBtnAddFriend.length > 0) {
+  listBtnAddFriend.forEach((button) => {
+    addFriend(button);
+  });
+}
+// end chức năng gửi yêu cầu
+
+// chức năng hủy yêu cầu
+const listBtnCancelFriend = document.querySelectorAll("[btn-cancel-friend]");
+if (listBtnCancelFriend.length > 0) {
+  listBtnCancelFriend.forEach((button) => {
+    cancelFriend(button);
+  });
+}
+// hêt chức năng hủy yêu cầu
+
+// chức năng chấp nhận yêu cầu
+const listBtnAcceptFriend = document.querySelectorAll("[btn-accept-friend]");
+if (listBtnAcceptFriend.length > 0) {
+  listBtnAcceptFriend.forEach((button) => {
+    acceptFriend(button);
+  });
+}
+// hết chức năng chấp nhận yêu cầu
+
+// chức năng từ chối yêu cầu
+const listBtnRefuseFriend = document.querySelectorAll("[btn-refuse-friend]");
+if (listBtnRefuseFriend.length > 0) {
+  listBtnRefuseFriend.forEach((button) => {
+    refuseFriend(button);
+  });
+}
+// hết chức năng từ chối yêu cầu
+
+// SERVER_RETURN_LENGTH_ACCEPT_FRIEND
+const badgeUsersAccept = document.querySelector("[badge-users-accept]");
+if (badgeUsersAccept) {
+  const usersId = badgeUsersAccept.getAttribute("badge-users-accept");
+  socket.on("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", (data) => {
+    if (usersId === data.usersId) {
+      badgeUsersAccept.innerHTML = data.lengthAcceptFriends;
+    }
+  });
+}
+// END LENGTH_ACCEPT
+
+// SERVER_RETURN_INFO_ACCEPT_FRIEND
+const dataUsersAccept = docu
---
+++ .history/sockets/client/users.socket_20231108200334.js
@@ -0,0 +1,204 @@
+const User = require("../../models/user.model");
+
+module.exports = (res) => {
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_ADD_FRIEND", async (usersId) => {
+      const myId = res.locals.user.id; // id của A
+
+      // biến thể hiện sự hiện diện id của A trong B cụ thể là trong trường accept
+      const existIdAinB = await User.findOne({
+        _id: usersId, // id của B
+        acceptFriends: myId,
+      });
+
+      // nếu không tìm thấy id của A trong accept B
+      if (!existIdAinB) {
+        await User.updateOne(
+          {
+            _id: usersId,
+          },
+          {
+            $push: { acceptFriends: myId },
+          }
+        );
+      }
+
+      // biến thể hiện sự hiện diện id của B trong trường requestFriends A
+      const existIdBinA = await User.findOne({
+        _id: myId,
+        requestFriends: usersId,
+      });
+
+      if (!existIdBinA) {
+        await User.updateOne(
+          {
+            _id: myId,
+          },
+          {
+            $push: { requestFriends: usersId },
+          }
+        );
+      }
+      // đứng từ A gửi lời mời cho B
+      // lấy ra độ dài acceptFriends của B và trả về cho B
+      const infoUserB = await User.findOne({
+        _id: usersId,
+      });
+      const lengthAcceptFriends = infoUserB.acceptFriends.length;
+
+      socket.broadcast.emit("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", {
+        usersId: usersId,
+        lengthAcceptFriends: lengthAcceptFriends,
+      });
+    });
+
+    socket.on("CLIENT_CANCEL_ADD_FRIEND", async (usersId) => {
+      const myId = res.locals.user.id;
+
+      // xóa id A trong trường acceptFriends của B
+      const existIdAinB = await User.findOne({
+        _id: usersId,
+        acceptFriends: myId,
+      });
+
+      if (existIdAinB) {
+        await User.updateOne(
+          {
+            _id: usersId,
+          },
+          {
+            $pull: { acceptFriends: myId },
+          }
+        );
+      }
+
+      // xóa id của B trong trường requestFriends của A
+      const existIdBinA = await User.findOne({
+        _id: myId,
+        requestFriends: usersId,
+      });
+
+      if (existIdBinA) {
+        await User.updateOne(
+          {
+            _id: myId,
+          },
+          {
+            $pull: { requestFriends: usersId },
+          }
+        );
+      }
+
+      // lấy ra độ dài acceptFriends của B và trả về cho B
+      const infoUserB = await User.findOne({
+        _id: usersId,
+      });
+
+      const lengthAcceptFriends = infoUserB.acceptFriends.length;
+      socket.broadcast.emit("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", {
+        usersId: usersId,
+        lengthAcceptFriends: lengthAcceptFriends,
+      });
+
+      // lấy info của A gửi cho B
+      const infoUserA = await User.findOne({
+        _id: myId,
+      }).select("id avatar fullName");
+
+      socket.broadcast.emit("SERVER")
+    });
+
+    socket.on("CLIENT_ACCEPT_FRIEND", async (usersId) => {
+      const myId = res.locals.user.id;
+
+      // A -> gửi yêu cầu cho B
+      // thêm user_id, room_chat_id của A vào friendList của B
+
+      // tìm user có myId
+      const existIdAinB = await User.findOne({
+        _id: myId,
+        acceptFriends: usersId,
+      });
+
+      // nếu có thì thêm 2 trường user_id và romchatid vào
+      if (existIdAinB) {
+        await User.updateOne(
+          {
+            _id: myId,
+          },
+          {
+            $push: {
+              friendList: {
+                user_id: usersId,
+                room_chat_id: "",
+              },
+            },
+            // xóa id của người kia trong acceptFriends
+            $pull: { acceptFriends: usersId },
+          }
+        );
+      }
+
+      const existIdBinA = await User.findOne({
+        _id: usersId,
+        acceptFriends: myId,
+      });
+
+      if (existIdBinA) {
+        await User.updateOne(
+          {
+            _id: usersId,
+          },
+          {
+            $push: {
+              friendList: {
+                user_id: myId,
+                room_chat_id: "",
+              },
+            },
+
+            $pull: {
+              requestFriends: myId,
+            },
+          }
+        );
+      }
+    });
+
+    socket.on("CLIENT_REFUSE_FRIEND", async (usersId) => {
+      const myId = res.locals.user.id;
+      // đứng từ B gửi lời mời tới A
+      // xóa id của A trong acceptFriends của B
+      const existIdAinB = await User.findOne({
+        _id: myId,
+        acceptFriends: usersId,
+      });
+      if (existIdAinB) {
+        await User.updateOne(
+          {
+            _id: myId,
+          },
+          {
+            $pull: { acceptFriends: usersId },
+          }
+        );
+      }
+
+      const existIdBinA = await User.findOne({
+        _id: usersId,
+        requestFriends: myId,
+      });
+
+      if (existIdAinB) {
+        await User.updateOne(
+          {
+            _id: usersId,
+          },
+          {
+            $pull: { requestFriends: myId },
+          }
+        );
+      }
+    });
+  });
+};
